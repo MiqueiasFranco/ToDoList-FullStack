7dd3b6361f78c5fbe4d53ca06242028fa5dcfaef
MiqueiasFranco <miqueiassantos1552@gmail.com>
2025-02-15 23:13:20 -0300

    criacao de middlewares e funcoes delete e update

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -8,11 +8,25 @@ const getALL = async (_request, response) => {
 }
 
 const createTask = async (request, response)=> {
-    const createdTask = await tasksModel.createTask()
+    const createdTask = await tasksModel.createTask(request.body)
+    return response.status(201).json(createdTask)
    
+}
+
+const deleteTask = async(request, response) => {
+    const {id} = request.params
+    await tasksModel.deleteTask(id)
+    return response.status(204).json()
+}
+const updatedTask = async (request, response) =>{
+    const {id} = request.params 
+    await tasksModel.updatedTask(id, request.body)
+    return response.status(204).json()
 }
 
 module.exports = {
     getALL,
     createTask,
+    deleteTask,
+    updatedTask,
 }
--- backend/src/modules/tasksmodules.js
+++ backend/src/modules/tasksmodules.js
@@ -10,11 +10,25 @@ const createTask = async (task)=> {
     const dateUTC = new Date(Date.now()).toUTCString()
     const query = 'INSERT INTO tasks (title, status, created_at) VALUES (?, ?, ?)'
     const [createdTask] = await conection.execute(query, [title, 'pendente', dateUTC])
-    return createdTask
+    return {insertId: createdTask.insertId}
 
+}
+
+const deleteTask = async (id) =>{
+    const removedTask = await conection.execute('DELETE FROM tasks WHERE id =?',[id])
+    return removedTask
+}
+
+const updatedTask = async (id, task) =>{
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?'
+    const {title, status} = task
+    const [updatedTask] = await conection.execute(query,[title, status, id])
+    return updatedTask
 }
 
 module.exports = {
     getALL,
     createTask,
+    deleteTask,
+    updatedTask,
 }
