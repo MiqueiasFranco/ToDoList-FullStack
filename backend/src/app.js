deb43f280b25c9bc3141ce8ec392c56ca0602174
MiqueiasFranco <miqueiassantos1552@gmail.com>
2025-03-06 23:01:29 -0300

    criação do front end integrado com a api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app= express();
 const router = require('./router')
+const cors = require('cors')
 
+const app= express();
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
-app.get('/tasks', (Request, Response) => Response.status(200).send('olá, mundo'));
 
 module.exports = app
